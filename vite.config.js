fd2c7fa9ce62796f82069791171b8aebd68af6fb
Ozvaldo12 <oc4188062@gmail.com>
2026-05-15 16:24:42 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,29 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
+
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+
+  // 👇 IMPORTANTE PARA GITHUB PAGES
+  base: '/Tarjetas/',
+
   plugins: [
     vue(),
     tailwindcss(),
-    VitePWA({ 
+
+    VitePWA({
       registerType: 'autoUpdate',
+
       manifest: {
         name: 'Mis Tarjetas Pro',
         short_name: 'Tarjetas',
         description: 'Gestor avanzado de tarjetas de crédito',
         theme_color: '#f8fafc',
+
         icons: [
           {
             src: 'pwa-192x192.png',
@@ -31,6 +39,7 @@ export default defineConfig({
       }
     })
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
